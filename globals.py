f5e15d8e6689378cf0fed891acbb64de97d458b5
sapir <sapirkoren@gmail.com>
2021-04-27 16:31:47 +0300

    changing folders final

---
+++ globals.py
@@ -0,0 +1,9 @@
+global scenario_num
+global scenario_col_num
+global par_num
+global par_ride_num
+global current_par
+global main_path
+global percent
+global list_count_rmssd  # list which contains the number of N (RR intervals) in all scenarios.
+global summary_table
